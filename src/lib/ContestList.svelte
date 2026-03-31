adceaf059124b73375dfe93d819875274ad077de
apochrome <apochrome@proton.me>
2026-03-31 10:11:03 +0800

    modified:   README.md 	modified:   bun.lock 	modified:   package.json 	modified:   src/app.css 	modified:   src/lib/ContestList.svelte 	new file:   src/lib/DiscordButton.svelte 	new file:   src/lib/GitHubButton.svelte 	modified:   src/lib/YearList.svelte 	new file:   src/lib/assets/icons/GitHub_Invertocat_Black.svg 	new file:   src/lib/assets/icons/GitHub_Invertocat_White.svg 	modified:   src/lib/components/ui/separator/index.ts 	modified:   src/lib/components/ui/separator/separator.svelte 	modified:   src/lib/pregen/files.json 	modified:   src/lib/pregen/problemNames.csv 	modified:   src/lib/pregen/problemNames.json 	modified:   src/routes/+layout.svelte 	modified:   src/routes/+page.svx 	modified:   src/routes/contests/spot/+page.svx 	modified:   src/styles/base.css 	new file:   static/contests/inpho/2024_A.pdf 	new file:   static/contests/inpho/2025_A.pdf 	new file:   static/contests/spot/2026.pdf 	new file:   static/contests/spot/2026_REP.pdf 	new file:   static/contests/spot/2026_S.pdf

--- src/lib/ContestList.svelte
+++ src/lib/ContestList.svelte
@@ -1,26 +1,27 @@
 <script lang="ts">
 	import { contests } from '$lib/pregen/contests';
+	import * as Card from './components/ui/card/index.ts';
 </script>
 
 {#each contests as contest (contest.id)}
 	<div class="mb-3">
 		<a href="/contests/{contest.id}">
-			<div
-				class="flex flex-row items-center gap-x-0.5 rounded-(--radius) bg-card px-3 py-4 text-card-foreground inset-ring-1 inset-ring-foreground/10 transition hover:bg-muted hover:inset-ring-2 sm:gap-x-4 sm:px-6"
+			<Card.Root
+				class="flex flex-row items-center gap-x-0.5 px-3 py-4 transition hover:bg-muted sm:gap-x-4 sm:px-6 "
 			>
-				<h3 class="hidden shrink-0 basis-23 translate-y-[1pt] text-center text-5xl sm:block">
+				<h3 class="hidden shrink-0 basis-23 translate-y-[1pt] p-0 text-center text-5xl sm:block">
 					{contest.icon}
 				</h3>
 				<div class="flex w-full flex-col gap-y-1">
 					<div class="flex flex-row items-center">
-						<h3 class="inline shrink-0 basis-13 text-center text-2xl sm:hidden">
+						<h3 class="inline shrink-0 basis-13 p-0 text-center text-2xl sm:hidden">
 							{contest.icon}
 						</h3>
 						<h3 class="inline py-0 text-card-foreground">{contest.name}</h3>
 					</div>
 					<span class="px-3 sm:p-0"> {contest.summary} </span>
 				</div>
-			</div>
+			</Card.Root>
 		</a>
 	</div>
 {/each}
---
+++ src/lib/GitHubButton.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button/index.ts';
+	import { mode } from 'mode-watcher';
+
+	import GitHubWhite from '$lib/assets/icons/GitHub_Invertocat_White.svg';
+	import GitHubBlack from '$lib/assets/icons/GitHub_Invertocat_Black.svg';
+</script>
+
+{#if mode.current == 'dark'}
+	<Button variant="outline" size="icon" href="https://github.com/apchrme/phoxiv" target="_blank"
+		><img alt="GitHub" src={GitHubWhite} class="scale-60 opacity-75" /></Button
+	>
+{:else}
+	<Button variant="outline" size="icon" href="https://github.com/apchrme/phoxiv" target="_blank"
+		><img alt="GitHub" src={GitHubBlack} class="scale-60 opacity-75" /></Button
+	>
+{/if}
