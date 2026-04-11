3f98cab870e2e0e2e0dcfb85802bb518e0859218
apochrome <apochrome@proton.me>
2026-04-11 18:31:23 +0800

    The bug was in isomorphic-DOMPurify. This is probably a known issue?

---
+++ src/routes/contests/[contest]/+page.ts
@@ -0,0 +1,11 @@
+import { error } from '@sveltejs/kit';
+import contests from '$lib/pregen/contests.json';
+import type { PageLoad } from './$types';
+
+export const load: PageLoad = async ({ params }) => {
+	if (!contests.find((i) => i.id == params.contest)) {
+		error(404, {
+			message: 'Not found'
+		});
+	}
+};
