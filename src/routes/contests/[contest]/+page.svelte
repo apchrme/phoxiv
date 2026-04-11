0a10618480e622ee3bdb09995d31ef6eb1b410ae
apochrome <apochrome@proton.me>
2026-04-11 18:31:23 +0800

    The bug was in isomorphic-DOMPurify. This is probably a known issue?

--- src/routes/contests/[contest]/+page.svelte
+++ src/routes/contests/[contest]/+page.svelte
@@ -3,26 +3,26 @@
 	let { params }: PageProps = $props();
 	import contests from '$lib/pregen/contests.json';
 	import SvelteSeo from 'svelte-seo';
-	import DOMPurify from 'isomorphic-dompurify';
 	let contest = $derived(contests.find((i) => i.id == params.contest));
+
+	// contest will definitely be found, as the page.ts will throw a 404 if it isn't.
 
 	import YearList from './YearList.svelte';
 </script>
 
-{#if contest}
-	<SvelteSeo
-		title={contest.name}
-		description="An archive of problems and solutions from the {contest.name}, in PDF format."
-		keywords="problems, solutions, olympiad, physics"
-	/>
+<SvelteSeo
+	title={contest.name}
+	description="An archive of problems and solutions from the {contest.name}, in PDF format."
+	keywords="problems, solutions, olympiad, physics"
+/>
 
-	<h1>{contest.name}</h1>
+<h1>{contest.name}</h1>
 
-	{#if contest?.descriptionHtml}
-		<div class="mb-4 max-w-none">
-			{@html DOMPurify.sanitize(contest.descriptionHtml)}
-		</div>
-	{/if}
+{#if contest?.descriptionHtml}
+	<div class="mb-4 max-w-none">
+		{@html contest.descriptionHtml}
+	</div>
+{/if}
 
-	<YearList contestId={contest.id} />
-{:else}{/if}
+<YearList contestId={contest.id} />
+	
