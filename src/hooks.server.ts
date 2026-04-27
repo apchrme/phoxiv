0d4c31f2d6a0690ccdacd6c984befa96ff7aa977
apchrme <107033218+apchrme@users.noreply.github.com>
2026-04-27 08:49:45 +0800

    Implement caching for GET request responses
    Add caching for successful GET requests

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -7,7 +7,13 @@ export const handle: Handle = async ({ event, resolve }) => {
 	} else {
 		throw new Error('Database unavailable');
 	}
-
+  
 	const response = await resolve(event);
+	
+	// Apply caching to all successful GET requests
+	if (event.request.method === 'GET' && response.status === 200) {
+    	response.headers.set('cache-control', 'public, max-age=3600');
+	}
+	
 	return response;
 };
