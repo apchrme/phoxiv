ea0da423d943043c248601fb337e055ffdc03dd1
apochrome <apochrome@proton.me>
2026-07-26 13:04:02 +0800

    allow SSR for SEO purposes (but this results in a less ideal UX)

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,4 +3,3 @@ export const load = ({ setHeaders }) => {
 		'cache-control': 'max-age=14400, must-revalidate, private'
 	});
 };
-export const ssr = false; // prevent flash of content from SSR before animations
